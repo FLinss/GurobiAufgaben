0cf4ff313f1a28f4cbfee9cc5c0f6b9edb428adb
FLinss <Florian.linss@gmx.de>
2020-04-01 16:34:35 +0200

    add comments

--- Vorlesungsbeispiele/Py Dateien/Bsp3b_OperativePPPMengen.py
+++ Vorlesungsbeispiele/Py Dateien/Bsp3b_OperativePPPMengen.py
@@ -15,22 +15,27 @@ J_max = 2
 I = range(I_max)
 J = range(J_max)
 
+# Initialisierung des Modells
 m = gp.Model()
 
+# Initialisierung der Variablen
 x={}
 for j in J:
     x[j] = m.addVar(obj=g[j], name="x"+str(j))
 #x = m.addVars(J_max, obj=g, name="x")
 
+# Definition der Zielfunktion
 m.modelSense = GRB.MAXIMIZE
 # m.setObjective = (gp.quicksum(g[i] * x[i] for i in I),GRB.MAXIMIZE)
 
+# Hinzufügen von Nebenbedingungen
 for i in I:
     m.addConstr(gp.quicksum(a[i][j] * x[j] for j in J) <= T[i], name="c"+str(i))
 #m.addConstrs(gp.quicksum(a[i][j] * x[j] for j in J) <= T[i] for i in I)
 
+# Optimierung
 m.optimize()
 
+# Ergebnisausgabe
 m.printAttr(GRB.Attr.X)
 m.printAttr(GRB.Attr.ObjVal)
-
