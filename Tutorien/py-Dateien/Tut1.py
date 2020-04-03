571ecd7f53252ee5bdc3de54c586ba5081e8e9f1
FLinss <Florian.linss@gmx.de>
2020-04-03 15:37:39 +0200

    add first tutorium

--- Tutorien/py-Dateien/Tut1.py
+++ Tutorien/py-Dateien/Tut1.py
@@ -5,26 +5,22 @@ from gurobipy import GRB
 m = gp.Model()
 
 # Initialisierung der Variablen
-xB = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x_B")
-xK = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x_K")
-xL = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x_L")
-xT = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x_T")
+xB = m.addVar(vtype=GRB.CONTINUOUS, ub=600, name="x_B")
+xK = m.addVar(vtype=GRB.CONTINUOUS, ub=200, name="x_K")
+xL = m.addVar(vtype=GRB.CONTINUOUS, ub=50, name="x_L")
+xT = m.addVar(vtype=GRB.CONTINUOUS, ub=150, name="x_T")
 
 # Definition der Zielfunktion
-m.setObjective(7*xB + 3.5*xK + 2.3*xL + 3.2*xT, GRB.MAXIMIZE)
+m.setObjective(4.5*xB + 3.5*xK + 2.3*xL + 3.2*xT, GRB.MAXIMIZE)
 
 # Hinzufügen von Nebenbedingungen
 m.addConstr(6*xB + 3*xK + 3*xL + 4*xT <= 3900,"nb_I")
 m.addConstr(2*xB + 2.5*xK + 2.5*xL + 3*xT <= 3900, "nb_II")
 m.addConstr(2.5*xB + 3*xK + 1.5*xL + 3*xT <= 3900, "nb_III")
-m.addConstr(xB <= 600)
-m.addConstr(xK <= 200)
-m.addConstr(xL <= 50)
-m.addConstr(xT <= 150)
 
 # Optimierung
 m.optimize()
 
 # Ergebnisausgabe
-m.printAttr(GRB.Attr.X)
 m.printAttr(GRB.Attr.ObjVal)
+m.printAttr(GRB.Attr.X)
